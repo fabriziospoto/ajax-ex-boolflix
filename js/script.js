88239a18222bbaada55a5d16b0bb1b25593c84c0
Fabrizio <fabriziospoto1@gmail.com>
2020-09-03 11:55:34 +0200

    MIlestone1
    MIlestone1

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@
 $( document ).ready(function(e) {
 
     $('#search-btn').click(function(){
+        //reset()
         $("#movie-details").html('');
         var userInput = $('#input').val();
         ricerca(userInput);
-        //$('input').val('');
+        $('input').val('');
     });
 
     $(document).keydown(function(e) {
         if (e.keycode == 13 || e.which == 13) {
+            //reset()
             $("#movie-details").html('');
             var userInput = $('#input').val();
             ricerca(userInput);
-            //$('input').val('');
+            $('input').val('');
         }
     });
 });
@@ -31,15 +33,10 @@ function ricerca(data) {
                 language: "it-IT"
             },
             success: function(risposta) {
-                //$("#movie-details").empty();
-                var movie = risposta.results;
-                //console.log(movie);
-                for (var i = 0; i < movie.length; i++) {
-                    var source = $("#entry-template").html();
-                    var template = Handlebars.compile(source);
-                    var context = movie[i];
-                    var html = template(context);
-                    $('#movie-details').append(html);
+                if(risposta.total_results > 0) {
+                    printFilm(risposta.results);
+                } else {
+                    noResults();
                 }
             },
             error: function() {
@@ -48,3 +45,34 @@ function ricerca(data) {
         }
     )
 }
+
+function printFilm(data) {
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    for (var i = 0; i < data.length; i++) {
+        var context = {
+            titolo: data[i].title,
+            original_title: data[i].original_title,
+            original_language: data[i].original_language,
+            vote_average: data[i].vote_average
+        };
+        var html = template(context);
+        $('#movie-details').append(html);
+    }
+}
+
+function noResults() {
+    var source = $("#no-results-template").html();
+    var template = Handlebars.compile(source);
+    var context = {
+        noResults: 'Non ci sono risultati'
+    };
+    var html = template(context);
+    $('#movie-details').append(html);
+}
+
+// function reset() {
+//     //$('#movie-details').val('')
+//     $('#movie-details').html('');
+//     $('#input').val('');
+// }
